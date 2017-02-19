6b23ce7b5e8f19dd1ea94f0cb619ece51a8e9bcd
Katy McCann <katy.alice.mccann@gmail.com>
2017-02-19 11:12:48 +0000

    Create top_up method

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -3,6 +3,10 @@ class Oyster
 attr_reader :amount_on_card
 
   def initialize
-    @amount_on_card = 0
+    @amount_on_card = 5
+  end
+
+  def top_up(amount)
+    @amount_on_card += amount
   end
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -3,6 +3,13 @@ require 'oyster'
 describe Oyster do
   let(:oyster) {Oyster.new}
   it 'has no money on the card' do
-    expect(oyster.amount_on_card).to eq 0
+    expect(oyster.amount_on_card).to eq 5
   end
+describe "#top_up" do
+  it 'can be topped up' do
+    oyster.top_up(5)
+    expect(oyster.amount_on_card).to eq 10
+  end
+end
+
 end
