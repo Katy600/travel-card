2f45f0409d44d694db9d21d3f78961b18eaade44
Katy McCann <katy.alice.mccann@gmail.com>
2017-02-19 16:59:55 +0000

    test method top_up responds with one argument

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -6,10 +6,19 @@ describe Oyster do
     expect(oyster.amount_on_card).to eq 5
   end
 describe "#top_up" do
+
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
   it 'can be topped up' do
     oyster.top_up(5)
     expect(oyster.amount_on_card).to eq 10
   end
+
+  it 'can top up the balance' do
+    expect{ oyster.top_up 1 }.to change{ oyster.amount_on_card }.by 1
+  end
+
 end
+describe "#maximum_top_up"
 
 end
